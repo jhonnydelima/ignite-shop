00c73af4073a0d15f84d3c2566c15cd3689b36ec
Jhonny Lima <jhonny.lima@funcionalcorp.com.br>
2024-04-01 15:20:39 -0300

    feat: add Carousel to Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useKeenSlider } from 'keen-slider/react';
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import shirt1 from '../assets/shirt01.png';
@@ -7,10 +8,19 @@ import shirt3 from '../assets/shirt03.png';
 import shirt4 from '../assets/shirt04.png';
 import shirt5 from '../assets/shirt05.png';
 
+import 'keen-slider/keen-slider.min.css';
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} width={520} height={480} alt="" />
 
         <footer>
@@ -19,11 +29,38 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta 02</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 03</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt4} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 04</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt5} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 05</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
