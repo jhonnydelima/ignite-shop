4956db09fb9a2099cd14a62af09865a716f74b3f
Jhonny Lima <jhonny.lima@funcionalcorp.com.br>
2024-04-02 16:21:50 -0300

    feat: create Success page structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Boa, <strong>Jhonny Lima</strong>! Sua <strong>Camiseta Beyond The Limits</strong> já está a caminha da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   );
 }
