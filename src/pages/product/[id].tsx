c8891c482efc314b5432180d5feaf20863f5233c
Jhonny Lima <jhonny.lima@funcionalcorp.com.br>
2024-04-02 16:02:02 -0300

    feat: add checkout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,9 +2,11 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 // import { useRouter } from "next/router";
 import Stripe from "stripe";
+import axios from "axios";
 
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 import { stripe } from "../../lib/stripe";
+import { useState } from "react";
 
 interface ProductProps {
   product: {
@@ -13,10 +15,39 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   };
 }
 
 export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false);
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      });
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl;
+
+      /**
+       * Caso o redirecionamento fosse para uma rota INTERNA:
+       * const router = useRouter()
+       * router.push('/checkout)
+       */
+    } catch (error) {
+      // Conectar à uma ferramenta de observabilidade (Datadog/Sentry)
+
+      setIsCreatingCheckoutSession(false);
+
+      alert('Falha ao redirecionar ao checkout!');
+    }
+  }
+
   // const { isFallback } = useRouter();
 
   // if (isFallback) {
@@ -37,7 +68,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
           Comprar agora
         </button>
       </ProductDetails>
@@ -77,6 +108,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, // 1 hour
