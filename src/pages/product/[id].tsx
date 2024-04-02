e754ede309c767b9e2ba0a70f76a67c698970f48
Jhonny Lima <jhonny.lima@funcionalcorp.com.br>
2024-04-02 11:08:14 -0300

    feat: create Product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
 import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est doloremque eos, ex a quaerat necessitatibus consequuntur ducimus harum aperiam impedit? Sed nulla iure culpa esse dolor commodi neque aspernatur perspiciatis!</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
